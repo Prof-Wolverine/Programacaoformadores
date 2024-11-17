ce5c8228a484f63093b64e336a37ec9a74696556
Prof-Wolverine <161845570+Prof-Wolverine@users.noreply.github.com>
2024-11-17 23:31:11 +0000

    17/11

--- app.js
+++ app.js
@@ -5,14 +5,13 @@ function criaCartao(categoria, pergunta, resposta) {
 
     cartao.innerHTML = `
     <div class="cartao__conteudo">
-    <h3>${categoria}</h3>
+        <h3>${categoria}</h3>
     </div>
     <div class="cartao__conteudo__pergunta">
         <p>${pergunta}</p>
     </div>
     <div class="cartao__conteudo__resposta">
         <p>${resposta}</p>
-    </div>
     </div>
     `
 }
