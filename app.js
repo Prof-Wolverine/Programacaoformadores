99fdfa0c5f6b2a60d1cdb7495858d0c2d6eedbf9
Prof-Wolverine <161845570+Prof-Wolverine@users.noreply.github.com>
2024-11-17 23:34:43 +0000

    17/11

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ function criaCartao(categoria, pergunta, resposta) {
         respostaEstaVisivel = !respostaEstaVisivel
         cartao.classList.toggle('active', respostaEstaVisivel)
     }
-    cartao.addEventListener('click', criaCartao)
+    cartao.addEventListener('click', viraCartao)
     
     let container = document.getElementById('container')
     container.appendChild(cartao)
